9c5443f4f635e51e8dedb0f49a75f5d9f3bf0cba
Alidaher02 <12132345@students.liu.edu.lb>
2025-09-10 00:44:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+const close = document.getElementById('closebtn');
+const socialIcons = document.getElementById('social-icons');
+
+btn.addEventListener('click', () => {
+  nav.classList.remove('hidden');
+
+  // Animate fade out
+  socialIcons.classList.remove('opacity-100', 'scale-100');
+  socialIcons.classList.add('opacity-0', 'scale-95');
+
+  // After animation ends, keep display flex but fully transparent & scaled down
+});
+
+close.addEventListener('click', () => {
+  nav.classList.add('hidden');
+
+  // Animate fade in
+  socialIcons.classList.remove('opacity-0', 'scale-95');
+  socialIcons.classList.add('opacity-100', 'scale-100');
+});
+ 
+var swiper = new Swiper(".slide_Product", {
+  slidesPerView: 5,
+  spaceBetween:20,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullests: true,
+    clickable:true
+  },
+  autoplay:{
+      delay:2500,
+  },
+  loop:true
+});
+
+
